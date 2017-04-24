7542f574f11f6c12adab8ee0d5bfb3a811f4284e
Qinka <qinka@live.com>
2017-04-24 22:48:47 +0800

    update

--- integration/travis-ci/before.sh
+++ integration/travis-ci/before.sh
@@ -52,6 +52,7 @@ export PATH=$HOME/.local/bin:$PATH
 travis_retry curl -L https://www.stackage.org/stack/linux-x86_64 \
     | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
 stack setup
+stack path
 
 ### End
 echo pre-install end
