25759c6ac36b2896b317b1539216fd56edf6ffc0
Qinka <qinka@live.com>
2017-04-25 13:52:54 +0800

    tmp

--- integration/travis-ci/after.sh
+++ integration/travis-ci/after.sh
@@ -12,15 +12,20 @@ if [ x"$DOCKER" = "xtrue" ]; then
     fi
     export OS_INFO=$(uname)-$OS_DISTRIBUTOR-$OS_CORENAME-GHC_$GHC_VER-$(uname -m)
     export IMAGE_TAG=$VERSION-$OS_INFO
+    export LATEST_TAG=latest-$OS_INFO
     if [ -n "$LLVM" ]; then
 	export IMAGE_TAG=$IMAGE_TAG-llvm-$LLVM
+	export LATEST_TAG=$LATEST_TAG-llvm-3.7
     fi
     if [ x"$THREADED" = "xtrue" ]; then
 	export IMAGE_TAG=$IMAGE_TAG-threaded
+	export LATEST_TAG=$LATEST_TAG-threaded
     fi
     if [ x"$DEBUG" = "xtrue" ]; then
 	export IMAGE_TAG=$IMAGE_TAG-debug
+	export LATEST_TAG=$LATEST_TAG-debug
 	export DEBUG_EXT=.debug
+	export DEBUG_LATEST_TAG=debug-latest$DLT_ID
     fi
     ### Coping files
     cd $TRAVIS_BUILD_DIR
@@ -30,5 +35,7 @@ if [ x"$DOCKER" = "xtrue" ]; then
     cp $TRAVIS_BUILD_DIR/integration/enters/entrypoint.py docker.tmp/Dockerfile
     ## Build
     docker build -t qinka/pim-be:$IMAGE_TAG .
+    docker tag qinka/pim-be:$IMAGE_TAG qinka/pim-be:$LATEST_TAG
+    docker tag qinka/pim-be:$IMAGE_TAG qinka/pim-be:$DEBUG_LATEST_TAG
     docker push qinka/pim-be
 echo After success (end)
